9197ac51ab61950064cb126104abc893018cc8ca
unknown <debby.sinkalu@wakanow.com>
2020-01-10 11:28:39 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundErrorsComponent } from './page-not-found-errors/page-not-found-errors.component';
+import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component : LoginComponent},
+  {path: 'change-password', component : ChangePasswordComponent},
+  {path: 'page-not-found-errors', component : PageNotFoundErrorsComponent},
+  {path: 'dashboard-admin', component : DashboardAdminComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule  { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+
+
+import { CustomMaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import 'hammerjs';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { PageNotFoundErrorsComponent } from './page-not-found-errors/page-not-found-errors.component';
+import{ DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';             
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardAdminComponent,          
+    ChangePasswordComponent,
+    PageNotFoundErrorsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
